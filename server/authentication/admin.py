63930170bf0ef7f71169a41167ed4c902e4de35b
Riley Cummins <bluemember1999@gmail.com>
2020-04-22 22:36:30 -0400

    Customize User model

--- server/authentication/admin.py
+++ server/authentication/admin.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from .models import User
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal Info'), {
+            'fields': ('first_name', 'last_name', 'email')
+        }),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+        }),
+        (_('Important dates'), {
+            'fields': ('last_login', 'date_joined')
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('first_name', 'last_name', 'email', 'password', 'role')
+        }),
+    )
+
+admin.site.register(User, UserAdmin)
+
--- server/authentication/models.py
+++ server/authentication/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+# Create User Model.
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('USER', 'USER'),
+        ('MANAGER', 'MANAGER'),
+        ('ADMIN', 'ADMIN'),
+    ]
+    email = models.EmailField(_("User's Email Address"), unique=True)
+    first_name = models.CharField(_("User's First Name"), max_length=20, blank=True, null=True)
+    last_name = models.CharField(_("User's Last Name"), max_length=20, blank=True, null=True)
+    role = models.CharField(_("Uers's Role"), max_length=10, choices=ROLE_CHOICES, default='USER')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    @property
+    def fullname(self):
+        return '{} {}'.format(self.first_name, self.last_name)
